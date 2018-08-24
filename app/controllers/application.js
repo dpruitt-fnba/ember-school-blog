48a648b9850cdd5ceb25d41b9dd958321635faf3
Dylan Pruitt <dpruitt@fnba.com>
2018-08-24 14:29:35 -0400

    Add date to create action

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,16 +6,19 @@ export default Controller.extend({
 
   newPostTitle: null,
   newPostBody: null,
+  newPostPublishDate: null,
 
   actions: {
     createPost() {
       this.store.createRecord('post', {
         title: this.get('newPostTitle'),
-        body: this.get('newPostBody')
+        body: this.get('newPostBody'),
+        publishDate: new Date(this.get('newPostPublishDate'))
       }).save();
 
       this.set('newPostTitle', null);
       this.set('newPostBody', null);
+      this.set('newPostPublishDate', null);
     },
     deletePost(post) {
       post.destroyRecord();
