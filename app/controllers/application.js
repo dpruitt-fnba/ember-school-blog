7a0fb6b90d3ceb757560cbcb7b24671738060534
Dylan Pruitt <dpruitt@fnba.com>
2018-08-24 13:57:32 -0400

    Add delete post action

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,5 +2,11 @@ import Controller from '@ember/controller';
 import { alias } from '@ember/object/computed';
 
 export default Controller.extend({
-  posts: alias('model')
+  posts: alias('model'),
+
+  actions: {
+    deletePost(post) {
+      post.destroyRecord();
+    }
+  }
 });
