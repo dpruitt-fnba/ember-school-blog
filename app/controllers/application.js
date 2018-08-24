e182cf0b933e1215fb2f48139e104f9af8e795d4
Dylan Pruitt <dpruitt@fnba.com>
2018-08-24 14:04:46 -0400

    Add create post action

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,7 +4,19 @@ import { alias } from '@ember/object/computed';
 export default Controller.extend({
   posts: alias('model'),
 
+  newPostTitle: null,
+  newPostBody: null,
+
   actions: {
+    createPost() {
+      this.store.createRecord('post', {
+        title: this.get('newPostTitle'),
+        body: this.get('newPostBody')
+      }).save();
+
+      this.set('newPostTitle', null);
+      this.set('newPostBody', null);
+    },
     deletePost(post) {
       post.destroyRecord();
     }
